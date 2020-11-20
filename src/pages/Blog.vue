c3c203221251336af616f96e173a4531fee67c3b
NPetz <souleisen@gmail.com>
2020-11-20 13:28:05 +0900

    changed padding bottom of blog to fix kasanari with navbar

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,25 +1,28 @@
 <template>
-<Layout>
+  <Layout>
     <div class="bg-yellow-100 min-h-screen">
-        <div class="flex flex-col divide-y divide-dotted divide-gray-800 gap-8 mx-auto py-12 px-6 max-w-sm md:max-w-xl">
-            <PostHeader v-for="edge in $page.posts.edges" :key="edge.node.id" :prop="{
-          title: edge.node.title,
-          date: edge.node.date,
-          id: edge.node.id,
-        }" />
-        </div>
+      <div class="flex flex-col divide-y divide-dotted divide-gray-800 gap-8 mx-auto py-12 pb-16 px-6 max-w-sm md:max-w-xl">
+        <PostHeader
+          v-for="edge in $page.posts.edges"
+          :key="edge.node.id"
+          :prop="{
+            title: edge.node.title,
+            date: edge.node.date,
+            id: edge.node.id,
+          }"
+        />
+      </div>
     </div>
-
-</Layout>
+  </Layout>
 </template>
 
 <script>
 import PostHeader from "~/components/PostHeader.vue";
 
 export default {
-    components: {
-        PostHeader,
-    },
+  components: {
+    PostHeader,
+  },
 };
 </script>
 
