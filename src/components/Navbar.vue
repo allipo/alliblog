ba1ce5eb215141072cd0c04072633cbe7f198fc2
NPetz <souleisen@gmail.com>
2020-11-23 12:32:52 +0900

    changed navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="font-serif">
-    <div
-      class="invisible md:visible fixed top-0 left-0 w-full min-h-screen md:w-48 bg-white"
-    >
+    <div class="invisible md:visible fixed top-0 left-0 w-full min-h-screen md:w-48 bg-white">
       <div class="mx-auto mt-16 mb-16">
         <g-link to="/">
           <g-image class="mx-auto drop dred" src="~/cute.png" width="150" />
         </g-link>
       </div>
 
-      <div
-        class="h-40 mt-16 mb-16 ml-8 pl-4 flex justify-between flex-col text-gray-800 text-lg border-l border-black"
-      >
+      <div class="h-40 mt-16 mb-16 ml-8 pl-4 flex justify-between flex-col text-gray-800 text-lg border-l border-black">
         <g-link class="link" to="/">Home</g-link>
         <g-link class="link" to="/blog">Blog</g-link>
         <g-link class="link" to="/gallery">Gallery</g-link>
@@ -19,25 +15,15 @@
       </div>
     </div>
 
-    <div
-      class="mx-auto md:hidden fixed bottom-0 inset-x-0 mb-4 z-30 w-12 sm:w-20"
-    >
-      <g-image
-        class="mx-auto flip drop dred"
-        src="~/cute.png"
-        width="100"
-        @click="showNav = !showNav"
-      />
+    <div class="mx-auto md:hidden fixed bottom-0 right-0 mb-5 mr-2 sm:mb-2 sm:mr-4 z-30 w-12 sm:w-20">
+      <g-image class="mx-auto flip drop dred" src="~/cute.png" width="100" @click="showNav = !showNav" />
     </div>
 
-    <div
-      v-if="this.showNav"
-      class="navbor gap-x-2 sm:gap-x-6 flex text-xs sm:text-base fixed md:hidden inset-x-0 bottom-0 mx-2 sm:mx-6 mb-6 justify-around z-20"
-    >
+    <div v-if="this.showNav" class="navbor gap-x-2 sm:gap-x-6 flex text-xs sm:text-base fixed md:hidden inset-x-0 bottom-0 mx-2 sm:mx-6 mb-6 justify-around z-20">
       <g-link class="link" to="/">Home</g-link>
       <g-link class="link" to="/blog">Blog</g-link>
-      <g-link class="ml-16 link" to="/gallery">Gallery</g-link>
-      <g-link class="link" to="/about">About</g-link>
+      <g-link class="link" to="/gallery">Gallery</g-link>
+      <g-link class="link mr-8 sm:mr-12" to="/about">About</g-link>
     </div>
   </div>
 </template>
